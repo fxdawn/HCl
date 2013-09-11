01c7d45619b3a9422d75cba3033e8eb66734b1e7
fxdawn <fxdawn@gmail.com>
2013-09-11 22:06:53 +0800

    initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 78)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(542, 273)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "在这里输入MAC地址，一行一个"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(287, 235)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(154, 32)
+        Me.Button4.TabIndex = 1
+        Me.Button4.Text = "保存"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.TextBox1.Location = New System.Drawing.Point(21, 22)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(499, 207)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(206, 364)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(165, 36)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "发送"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(399, 364)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(142, 36)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "退出"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(88, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(187, 15)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "师大网络中心你节操呢！！"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(339, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "本机IP："
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(176, 52)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(231, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "bug report: fxdawn@gmail.com"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(42, 364)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(130, 36)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "使用说明"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(105, 235)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(149, 32)
+        Me.Button5.TabIndex = 2
+        Me.Button5.Text = "清空"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(589, 415)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "HCl"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,140 @@
+﻿Imports System.Windows.Forms
+Imports System.Text
+Imports System.Net.Sockets
+Imports System.Net
+Imports System
+Imports System.Diagnostics
+Imports System.Net.NetworkInformation
+Imports System.Text.RegularExpressions
+Imports System.Management
+Imports System.IO
+Public Class Form1
+    Dim k As String
+    Dim r As String
+    Dim b As Object
+    Private Property str As String
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x As Array
+        Dim i As Int32 = 0
+        Dim stritem As String
+        x = Split(TextBox1.Text, vbCrLf)
+        For Each stritem In x
+            i = i + 1
+            If Not Regex.IsMatch(stritem.ToString(), "^([0-9a-fA-F]{2})(([/\s:][0-9a-fA-F]{2}){5})$") Then
+                If Not Regex.IsMatch(stritem.ToString(), "^$") Then
+                    MsgBox("第" & i & "行的MAC地址有误，请检查")
+                End If
+            End If
+            Try
+                Dim s As New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+                Dim localEndPoint As New IPEndPoint(IPAddress.Parse("202.193.160.123"), 20015)
+                'Dim localEndPoint As New IPEndPoint(IPAddress.Parse("192.168.2.14"), 20015)
+                s.Connect(localEndPoint)
+                Dim byte1(), byte2(), byte3(), byte4() As Byte
+                byte1 = {&H32, &H33, &H33, &H33, &H33, &H33, &H33, &H33, &H33, &H33, &H33, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &HAC, &H13, &H3B, &H7C}
+                byte2 = System.Text.Encoding.UTF8.GetBytes(stritem)
+                byte3 = {&H0, &H0, &H0, &H2}
+                ReDim byte4(byte1.Length + byte2.Length + byte3.Length)
+                byte1.CopyTo(byte4, 0)
+                byte2.CopyTo(byte4, byte1.Length)
+                byte3.CopyTo(byte4, byte1.Length + byte2.Length)
+                s.Send(byte4)
+                s.Close()
+            Catch ex As Exception
+            End Try
+        Next
+        MsgBox("数据已发送。" & vbCrLf & "连接网络时直接拨号就好了，不需要使用万恶的出校控制器。")
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = GetMac()
+        Dim ipaddr As String
+        ipaddr = GetIpV4()
+        Label2.Text = "当前IP：" & GetIpV4()
+    End Sub
+    Private Sub Socket(p1 As Object)
+        Throw New NotImplementedException
+    End Sub
+    Public Function StrToHex(ByRef Data As String) As String
+        'Dim B() As Array
+        Dim sVal As String
+        Dim sHex As String = ""
+        While Data.Length > 0
+            sVal = Conversion.Hex(Strings.Asc(Data.Substring(0, 1).ToString()))
+            Data = Data.Substring(1, Data.Length - 1)
+            sHex = sHex & sVal
+        End While
+        Return sHex
+    End Function
+    Public Function GetMac() As String
+        Try
+            Dim stringMAC As String = ""
+            Dim stringIP As String = ""
+            Dim query As ManagementObjectSearcher = New ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapterConfiguration")
+            Dim queryCollection As ManagementObjectCollection = query.Get
+            For Each mo As ManagementObject In queryCollection
+                If CBool(mo("IPEnabled").ToString) = True Then
+                    stringMAC += mo("MACAddress").ToString()
+                End If
+            Next
+            Return stringMAC
+        Catch ex As Exception
+        End Try
+        Return String.Empty
+    End Function
+
+    Public Function GetIpV4() As String
+        '有可能会返回IPv6的地址
+        '于是就用了GetIPv4
+        Dim myHost As String = Dns.GetHostName
+        Dim ipEntry As IPHostEntry = Dns.GetHostEntry(myHost)
+        Dim ip As String = ""
+
+        For Each tmpIpAddress As IPAddress In ipEntry.AddressList
+            If tmpIpAddress.AddressFamily = Sockets.AddressFamily.InterNetwork Then
+                Dim ipAddress As String = tmpIpAddress.ToString
+                ip = ipAddress
+                Exit For
+            End If
+        Next
+
+        If ip = "" Then
+            Throw New Exception("No 10. IP found!")
+        End If
+
+        Return ip
+
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Form2.Show()
+    End Sub
+    Function GetEnvironmentVariable()
+
+        Dim strComputerName As String
+
+        strComputerName = Environment.GetEnvironmentVariable("UserProFile")
+
+        Return (strComputerName)
+
+    End Function
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim IntR As Integer
+        IntR = MsgBox("确认清空？", vbYesNo, "清空确认")
+        If IntR = vbYes Then
+            TextBox1.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim sw As System.IO.StreamWriter
+        sw = New System.IO.StreamWriter(GetEnvironmentVariable() & "\desktop\MAC地址.txt", True)
+        sw.WriteLine(TextBox1.Text)
+        sw.Close()
+        MsgBox("已保存到桌面""MAC地址.txt""文件。" & vbCrLf & "粘贴到获取到了172地址的电脑上发送数据即可连接。")
+    End Sub
+
+End Class
